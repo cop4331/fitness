8ee31fd9e20131c7191f1cbd129d8ddbce235517
noahavi <54644728+noahavi@users.noreply.github.com>
2020-03-25 21:12:33 -0400

    authentication

--- fitness/fitness/lib/signin.dart
+++ fitness/fitness/lib/signin.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'mainpage.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart';
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget
 {
@@ -9,6 +14,43 @@ class LoginPage extends StatefulWidget
 
 class _LoginPageState extends State<LoginPage>
 {
+  bool _isLoading = false;
+
+  final TextEditingController userController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  doLogin(String username, password) async
+  {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    Map data = {
+      'email': username,
+      'password': password
+    };
+
+    var jsonResponse = null;
+    var response = await http.post("signinapi", body: data);
+    if (response.statusCode == 200)
+    {
+      jsonResponse = json.decode(response.body);
+
+      if (jsonResponse != null)
+      {
+        setState(() {
+          _isLoading = false;
+         });
+        sharedPreferences.setString("token", jsonResponse['token']);
+        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => MainPage()), (Route<dynamic> route) => false);
+      } 
+    }
+    else
+    {
+      setState(() {
+        _isLoading = false;
+      });
+      print(response.body);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -130,24 +172,3 @@ class _LoginPageState extends State<LoginPage>
         ));
   }
 }
-
-/*
-void signIn(String username, String password) async
-{
-  Map data = {
-    'username': username,
-    'password': password
-  };
-  var jsonData = null;
-  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-  // need api endpoint for post request
-  var response = await http.post("", body: data);
-
-  if (response.statusCode == 200)
-    {
-      jsonData = jsonDecode(response.body);
-      //set state below
-    }
-
-}
-*/
