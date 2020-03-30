216e34f8c10dcd46976c77744f2a544fce2ec5c2
noahavi <54644728+noahavi@users.noreply.github.com>
2020-03-30 16:33:20 -0400

    step counter almost done

--- fitness/fitness/lib/mainpage.dart
+++ fitness/fitness/lib/mainpage.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'dart:math';
-import 'dart:async';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-import 'package:intl/intl.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'signin.dart';
-
-
 
 class MainPage extends StatefulWidget
 {
@@ -18,24 +10,6 @@ class MainPage extends StatefulWidget
 
 class _MainPageState extends State<MainPage>
 {
-  
-  SharedPreferences sharedPreferences;
-  
-  checkLoginStatus() async
-  {
-    sharedPreferences = await SharedPreferences.getInstance();
-    if (sharedPreferences.getString("token") == null)
-    {
-      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (BuildContext context) => LoginPage()), (Route<dynamic> route) => false);
-    }
-  }
-  
-  String date()
-  {
-    var now = new DateTime.now();
-    return new DateFormat("EEEE, d LLLL").format(now);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -58,7 +32,7 @@ class _MainPageState extends State<MainPage>
                 borderRadius: BorderRadius.circular(50),
               ),
               tabs: [
-                Tab(text: "MAIN"),
+                Tab(icon: Icon(Icons.favorite, color: Colors.pink)),
                 Tab(text: "DIET"),
                 Tab(text: "WEIGHT"),
               ],
@@ -78,9 +52,7 @@ class _MainPageState extends State<MainPage>
                     padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
                     child: IconButton(
                       icon: Icon(Icons.exit_to_app, color: Colors.black),
-                      onPressed: (){
-                        //doLogout();
-                      },
+                      onPressed: (){},
                       iconSize: 30,
                     ),
                   ),
@@ -90,138 +62,10 @@ class _MainPageState extends State<MainPage>
           ),
           body: TabBarView(
             children: <Widget>[
-              Column(
-                children: <Widget>[
-                  SizedBox(width: 400,height: 20),
-                  Container(
-                  height: 200,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                      colors: [Colors.greenAccent, Colors.lightGreenAccent],
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: CircularPercentIndicator(
-                    radius: 180,
-                    lineWidth: 10,
-                    animation: true,
-                    center: Container(
-                     child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          Container(
-                            padding: EdgeInsets.only(top: 50),
-                            child: Text("0",
-                            style: TextStyle(
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold,
-                            )),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 5),
-                            child: Text("Steps",
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.bold,
-                            )),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(top: 20),
-                            child: Text("Goal: 500",
-                            style: TextStyle(
-                              fontSize: 15,
-                            )),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),  
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 5),
-                    child: Text(date().toUpperCase(),
-                    style: TextStyle(
-                      fontSize: 15,
-                    )),
-                  ),
-                  Container(
-                    padding: EdgeInsets.fromLTRB(110, 25, 90, 25),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            Text('0',
-                            style: TextStyle(
-                              fontSize: 40,
-                            )),
-                            Text("Miles"),
-                            Text("")
-                          ],
-                        ),
-                        Container(
-                          height: 80,
-                          child: VerticalDivider(
-                            color: Colors.black,
-                            width: 40,
-                            thickness: 1,
-                          ),
-                          
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text('0',
-                            style: TextStyle(
-                              fontSize: 40,
-                            )),
-                            Text("Calories"),
-                            Text("burned")
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 20),
-                   child: Row(
-                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                     children: <Widget>[
-                       RaisedButton(
-                         onPressed: (){},
-                         color: Colors.green,
-                         shape: RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(25),
-                         ),
-                         child: Text("GOALS"),
-                       ),
-                        RaisedButton(
-                         onPressed: (){},
-                         color: Colors.green,
-                         shape: RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(25),
-                         ),
-                         child: Text("other stuff"),
-                       ),
-                       RaisedButton(
-                         onPressed: (){},
-                         color: Colors.green,
-                         shape: RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(25),
-                         ),
-                         child: Text("idk"),
-                       ),
-                     ],
-                   ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.only(top: 20),
-                    child: Text("Thinking of what to put down here, maybe graphs.\n This is just the first draft"),
-                  )
-                ],
-              ),
+              Text("Main Page area. Has step counter, distance traveled, graphs, goals, favorites, etc.",
+              style: TextStyle(
+                fontSize: 25,
+              )),
               Text("Dieting tab",
               style: TextStyle(
                 fontSize: 25,
